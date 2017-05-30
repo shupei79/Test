29a75a69216f43c12070bdd37838790ae2c3bc9a
shupei <shupei@naver.com>
2017-05-30 19:33:22 +0900

    프로젝트 생성

---
+++ LineageM_Enchant/LineageM_Enchant/Assets/Scripts/Common/Defines.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneState
+{
+    None = -1,
+    Title,
+    Lobby,
+    Battle,
+}
+
+public static class Version
+{
+    public const string value = "1.0.0";
+}
