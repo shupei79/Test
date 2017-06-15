b537c08fe08168cb81cb8b2e3054633e0f5ea122
shupei <shupei@naver.com>
2017-06-15 12:53:47 +0900

    아놔 .gitignore요게 문제 엿넴

---
+++ LineageM_Enchant/LineageM_Enchant/Assets/Scripts/Util/UI/Resolution/UIRatioFixer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIRatioFixer : MonoBehaviour
+{
+    [SerializeField]
+    private float allowedError = 0.0f;
+    
+    private void Start()
+    {
+        useGUILayout = false;
+        RectTransform rectTransform = transform as RectTransform;
+        CanvasScaler scaler = GetComponentInParent<CanvasScaler>();
+        if (scaler == null)
+        {
+            Debug.LogError("Could not find a CanvasScaler component in parent.");
+            return;
+        }
+        Vector2 referenceResolution = scaler.referenceResolution;
+        float wph1 = referenceResolution.x / referenceResolution.y;
+        float wph2 = (float)Screen.width / Screen.height;
+        if (Mathf.Abs(wph1 - wph2) > allowedError)
+        {
+            Vector3 scale = rectTransform.localScale;
+            scale.x *= wph2 / wph1;
+            rectTransform.localScale = scale;
+        }
+    }
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        SetSizeDeltaToReferenceResolution(transform);
+    }
+
+    public static void SetSizeDeltaToReferenceResolution(Transform transform_)
+    {
+        RectTransform rectTransform = transform_ as RectTransform;
+        CanvasScaler scaler = transform_.GetComponentInParent<CanvasScaler>();
+        if (scaler == null)
+        {
+            Debug.LogError("Could not find a CanvasScaler component in parent.");
+            return;
+        }
+        rectTransform.sizeDelta = scaler.referenceResolution;
+    }
+#endif
+}
---
+++ LineageM_Enchant/LineageM_Enchant/Assets/Scripts/Util/UI/Resolution/UIResolutionFixer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIResolutionFixer : MonoBehaviour
+{
+	private void Start()
+    {
+        RectTransform rectTransform = transform as RectTransform;
+        CanvasScaler scaler = GetComponentInParent<CanvasScaler>();
+        if (scaler == null)
+        {
+            Debug.LogError("Could not find a CanvasScaler component in parent.");
+            return;
+        }
+        Vector2 size = rectTransform.sizeDelta;
+        Vector2 referenceResolution = scaler.referenceResolution;
+        float standardRatio = referenceResolution.x / referenceResolution.y;
+        float screenRatio = (float)Screen.width / Screen.height;
+        size.x *= screenRatio / standardRatio;
+        rectTransform.sizeDelta = size;
+    }
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        UIRatioFixer.SetSizeDeltaToReferenceResolution(transform);
+    }
+#endif
+}
